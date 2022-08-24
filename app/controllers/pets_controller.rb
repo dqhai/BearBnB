b6ffd15c57decdac4cc585376ba15a6c89147faf
Q Hai Dang <haiqd@pm.me>
2022-08-24 12:27:01 +0200

    removed raise

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -14,7 +14,6 @@ class PetsController < ApplicationController
   def create
     @pet = Pet.new(pet_params)
     @pet.user = current_user
-    raise
     if @pet.save
       redirect_to pet_path(@pet)
     else
