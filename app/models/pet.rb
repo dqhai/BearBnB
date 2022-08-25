f1b01b7eaa13d462e819f9c31d97de2e3598e1ae
Afolabi Afolayan <aaafolayan@gmail.com>
2022-08-25 14:54:23 +0200

    made a confirmation message for bookings and made a searh bar

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,7 +1,15 @@
 class Pet < ApplicationRecord
   has_one_attached :photo
-  has_many :bookings
+  has_many :bookings, dependent: :destroy
   # has_many :users, through: :bookings
 
   belongs_to :user
+
+  include PgSearch::Model
+
+  pg_search_scope :search_by_name_and_species,
+                  against: %i[name species],
+                  using:{
+                    tsearch: { prefix: true }
+                  }
 end
